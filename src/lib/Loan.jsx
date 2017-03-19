ef6bee1f42c655e6676e59a09d625beda798b9b6
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2017-03-19 20:36:47 +0000

    refactor and add loan values

--- src/lib/Loan.jsx
+++ src/lib/Loan.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
+import LoanValues from './LoanValues.jsx';
+
 export default class Loan extends React.Component {
 
   render() {
+    let { annualisedReturn, ltv: loanToValue } = this.props;
+    const props = {
+      annualisedReturn: annualisedReturn,
+      loanToValue: loanToValue
+    };
     return (
       <div>
         <h2>{this.props.title}</h2>
         <p>Tranche: {this.props.tranche}</p>
         <p>Amount: {this.props.amount}</p>
+        <LoanValues {...props} />
       </div>
     );
   }
--- test/lib/Loan.spec.js
+++ test/lib/Loan.spec.js
@@ -3,15 +3,18 @@ import {shallow} from 'enzyme';
 import {expect} from 'chai';
 
 import Loan from '../../src/lib/Loan.jsx';
+import LoanValues from '../../src/lib/LoanValues.jsx';
 
 describe('<Loan/>', function() {
 
   const defaultProps = {
     title: "",
-    tranche: ""
+    tranche: "",
+    amount: 0,
+    annualisedReturn: 0
   };
 
-  it('should contain the title of the loan', function() {
+  it('should contain the title', function() {
     const props = { ...defaultProps, title: "test" }
     const wrapper = shallow(<Loan {...props} />);
     expect(wrapper.children().at(0).contains(
@@ -19,18 +22,29 @@ describe('<Loan/>', function() {
     )).to.equal(true);
   });
 
-  it('should contain the tranche of the loan', function() {
+  //having problems with enzyme's contain assertion, using string assertion below instead
+  it('should contain the tranche', function() {
     const props = { ...defaultProps, tranche: "test" }
     const wrapper = shallow(<Loan {...props} />);
-    //having problems with enzyme's contain assertion, using string assertion instead
     expect(wrapper.children().at(1).html()).to.equal("<p>Tranche: test</p>");
   });
 
-  it('should contain the amount of the loan', function() {
+  it('should contain the amount', function() {
     const props = { ...defaultProps, amount: 99999 }
     const wrapper = shallow(<Loan {...props} />);
-    //having problems with enzyme's contain assertion, using string assertion instead
     expect(wrapper.children().at(2).html()).to.equal("<p>Amount: 99999</p>");
+  });
+
+  it('should contain the <LoanValues/>', function() {
+    const props = { ...defaultProps }
+    const wrapper = shallow(<Loan {...props} />);
+    expect(wrapper.find(LoanValues).exists()).to.equal(true);
+  });
+
+  it('should pass the annualisedReturn and loanToValue through to <LoanValues/>', function() {
+    const props = { ...defaultProps }
+    const wrapper = shallow(<Loan {...props} />);
+    expect(wrapper.find(LoanValues).props()).to.have.all.keys(['annualisedReturn', 'loanToValue']);
   });
 
 });
