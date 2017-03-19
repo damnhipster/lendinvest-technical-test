752eec2a6f89d8181865e14c569dc920caa401b6
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2017-03-19 20:00:47 +0000

    hardcoded stub loan data

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import CurrentLoans from './lib/CurrentLoans.jsx';
 
 const defaultProps = {
   heading: 'Current Loans',
-  loans: []
+  loans: [{"id":"1","title":"Voluptate et sed tempora qui quisquam.","tranche":"A","available":"11,959","annualised_return":"8.60","term_remaining":"864000","ltv":"48.80","amount":"85,754"},{"id":"5","title":"Consectetur ipsam qui magnam minus dolore ut fugit.","tranche":"B","available":"31,405","annualised_return":"7.10","term_remaining":"1620000","ltv":"48.80","amount":"85,754"},{"id":"12","title":"Dolores repudiandae ut voluptas unde laborum quaerat et sapiente.","tranche":"C","available":"12,359","annualised_return":"4.80","term_remaining":"879000","ltv":"48.80","amount":"85,754"}]
 };
 
 ReactDOM.render(<CurrentLoans {...defaultProps}/>, document.getElementById('main'));
