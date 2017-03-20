dcdaa37c14dfaa2d7e9abb122e72ab715f8bfe65
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2017-03-20 02:05:22 +0000

    removed cssnext

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   plugins: [
-    require('precss')({ /* ...options */ }),
-    require('postcss-cssnext')({ /* ...options */ })
+    require('precss')({ /* ...options */ })
   ]
 }
