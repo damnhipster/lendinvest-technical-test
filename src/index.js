a41d2dc94944b02100bd0fe91a465ec6db957811
Hem Brahmbhatt <hamant.brahmbhatt@gmail.com>
2017-03-20 01:05:55 +0000

    fetching loan data from API, with a fallback

--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import ReactDOM from 'react-dom';
 import styles from './css/style.css';
 import CurrentLoans from './lib/CurrentLoans.jsx';
 
-const defaultProps = {
-  heading: 'Current Loans',
-  loans: [{"id":"1","title":"Voluptate et sed tempora qui quisquam.","tranche":"A","available":"11,959","annualised_return":"8.60","term_remaining":"864000","ltv":"48.80","amount":"85,754"},{"id":"5","title":"Consectetur ipsam qui magnam minus dolore ut fugit.","tranche":"B","available":"31,405","annualised_return":"7.10","term_remaining":"1620000","ltv":"48.80","amount":"85,754"},{"id":"12","title":"Dolores repudiandae ut voluptas unde laborum quaerat et sapiente.","tranche":"C","available":"12,359","annualised_return":"4.80","term_remaining":"879000","ltv":"48.80","amount":"85,754"}]
-};
+function setup({ loans = [{"id":"1","title":"Voluptate et sed tempora qui quisquam.","tranche":"A","available":"11,959","annualised_return":"8.60","term_remaining":"864000","ltv":"48.80","amount":"85,754"},{"id":"5","title":"Consectetur ipsam qui magnam minus dolore ut fugit.","tranche":"B","available":"31,405","annualised_return":"7.10","term_remaining":"1620000","ltv":"48.80","amount":"85,754"},{"id":"12","title":"Dolores repudiandae ut voluptas unde laborum quaerat et sapiente.","tranche":"C","available":"12,359","annualised_return":"4.80","term_remaining":"879000","ltv":"48.80","amount":"85,754"}] }) {
+  const defaultProps = {
+    heading: 'Current Loans',
+    loans: loans
+  };
+  ReactDOM.render(<CurrentLoans {...defaultProps}/>, document.getElementById('main'));
+}
 
-ReactDOM.render(<CurrentLoans {...defaultProps}/>, document.getElementById('main'));
+if(self.fetch) {
+  fetch('/api/current-loans.json')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      setup(json.loans);
+    });
+} else {
+  setup();
+}
